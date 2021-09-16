ddfedb48bf0fdf55a8682cf2830bf2cf025a47f0
vova <vovamushii@gmail.com>
2021-09-16 08:58:33 +0300

    test

--- report/js/script2.js
+++ report/js/script2.js
@@ -4,24 +4,24 @@ let btn21 = document.getElementById("2btn1").addEventListener("click", function(
 });
 
 let btn22 = document.getElementById("2btn2").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab1/lab1info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
 let btn23 = document.getElementById("2btn3").addEventListener("click", function() {
-    canvas.setAttribute("src", 'labsFrames/lab1/lab1table.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
 let btn24 = document.getElementById("2btn4").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab1/lab1form.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
 let btn25 = document.getElementById("2btn5").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab1/lab1img.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
 let btn26 = document.getElementById("2btn6").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab1/lab1conclusion.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
 
 let btn27 = document.getElementById("2btn7").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab1/lab1link.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
 let btn28 = document.getElementById("2btn8").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab1/photo.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
 });
