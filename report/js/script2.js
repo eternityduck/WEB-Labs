38aeb58d6b86cf036ee2713b103d8baf297c33b6
volodymyr <vovamushii@gmail.com>
2021-09-22 23:07:21 +0300

    lab2

--- report/js/script2.js
+++ report/js/script2.js
@@ -4,24 +4,27 @@ let btn21 = document.getElementById("2btn1").addEventListener("click", function(
 });
 
 let btn22 = document.getElementById("2btn2").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2list.html')
 });
 let btn23 = document.getElementById("2btn3").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2identificators.html')
 });
 let btn24 = document.getElementById("2btn4").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2retreats.html')
 });
 let btn25 = document.getElementById("2btn5").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2elements.html')
 });
 let btn26 = document.getElementById("2btn6").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2tagselector.html')
 });
 
 let btn27 = document.getElementById("2btn7").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2otherselectors.html')
 });
 let btn28 = document.getElementById("2btn8").addEventListener("click", function() {
-    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2info.html')
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2group.html')
+});
+let btn29 = document.getElementById("2btn9").addEventListener("click", function() {
+    canvas.setAttribute("src", '/report/labsFrames/lab2/lab2conclusion.html')
 });
