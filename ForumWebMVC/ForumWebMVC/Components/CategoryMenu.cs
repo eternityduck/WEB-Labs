1a0d18a6e76ebeb2e9178eb91388f67b2ec84a73
bohdan <bogdan2003bk96@gmail.com>
2021-12-08 22:37:27 +0200

    НИКОЛАЙ ДИКАРЕВ МОЛОДЕЦ

---
+++ ForumWebMVC/ForumWebMVC/Components/CategoryMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumWebMVC.Components
+{
+    public class CategoryMenu:ViewComponent
+    {
+        private readonly ITopicService _topicService;
+
+        public CategoryMenu(ITopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _topicService.GetAllAsync().Result.OrderBy(x => x.Title);
+            return View(categories);
+        }
+    }
+}
